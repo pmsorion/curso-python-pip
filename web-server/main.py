2af9afa3282054f0fe974bb10e219b60362e6c27
pmsorion <garciafran@hotmail.com>
2023-09-20 20:44:50 -0500

    add fastapi

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,20 @@
 from store import get_categories
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1, 2, 3, 4]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return '''
+            <h1>Hola soy una pagina</h1>
+            <p>soy un parrafo</p>
+'''
+
 
 def run():
     get_categories()
